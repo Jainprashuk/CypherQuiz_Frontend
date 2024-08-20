b9cf13b5bedd6c962929b9bff57b95e954aad0f2
prashuk jain <29jainprashuk@gmail.com>
2024-08-20 13:45:01 +0530

    files uploaded

---
+++ src/Components/ProtectedRoutes.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+const ProtectedRoutes = ({ children }) => {
+    // console.log("i am here");
+    
+  const token = document.cookie.includes('token'); 
+
+  console.log(token);
+  
+
+  return token ? children : <Navigate to="/login" />;
+};
+
+export default ProtectedRoutes;

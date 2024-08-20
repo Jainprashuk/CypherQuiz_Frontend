737a82cec146381b24157458623c1e5a4f9a0756
prashuk jain <29jainprashuk@gmail.com>
2024-08-20 18:19:07 +0530

    protected session

--- src/Components/ProtectedRoutes.jsx
+++ src/Components/ProtectedRoutes.jsx
@@ -2,22 +2,20 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 const ProtectedRoutes = ({ children }) => {
-    console.log("i am here");
-    
-  const token = document.cookie.includes('token'); 
+  console.log("i am here");
+
+  // Retrieve the token from sessionStorage
+  const token = sessionStorage.getItem('token'); 
 
   console.log(token);
 
+  // If token exists, render the children (protected routes), otherwise redirect to login
   if (token) {
     return children;
-  }else{
+  } else {
     console.log("yeh he dikkat");
-    
-    return <Navigate to="/login" />
+    return <Navigate to="/login" />;
   }
-  
-
-  // return token ? children : <Navigate to="/login" />;
 };
 
 export default ProtectedRoutes;

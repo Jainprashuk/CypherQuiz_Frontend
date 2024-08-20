744bc46804001d26a657ab9f05a8c06670287a8b
prashuk jain <29jainprashuk@gmail.com>
2024-08-20 14:32:22 +0530

    add

--- src/Components/ProtectedRoutes.jsx
+++ src/Components/ProtectedRoutes.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 const ProtectedRoutes = ({ children }) => {
-    // console.log("i am here");
+    console.log("i am here");
     
   const token = document.cookie.includes('token'); 
 
